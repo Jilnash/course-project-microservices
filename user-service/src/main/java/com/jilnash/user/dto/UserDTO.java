7663f137b5b923390dc62cff78826c66ef90cee9
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-06-28 00:24:48 +0500

    calling builder pattern in mapper toDto, specifying if user id is present in toEntity
    Signed-off-by: = <=>

--- user-service/src/main/java/com/jilnash/user/dto/UserDTO.java
+++ user-service/src/main/java/com/jilnash/user/dto/UserDTO.java
@@ -3,8 +3,10 @@ package com.jilnash.user.dto;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
 import lombok.Data;
 
+@Builder
 @Data
 public class UserDTO {
 
--- user-service/src/main/java/com/jilnash/user/mapper/UserMapper.java
+++ user-service/src/main/java/com/jilnash/user/mapper/UserMapper.java
@@ -2,24 +2,48 @@ package com.jilnash.user.mapper;
 
 import com.jilnash.user.dto.UserDTO;
 import com.jilnash.user.model.User;
+import com.jilnash.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
 
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
     public UserDTO toDTO(User user) {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setLogin(user.getLogin());
-        userDTO.setEmail(user.getEmail());
-//        userDTO.setPhone(user.getPhone());
-        return userDTO;
+
+        return UserDTO.builder()
+                .id(user.getId())
+                .login(user.getLogin())
+                .email(user.getEmail())
+                .phone(user.getPhone())
+                .build();
     }
 
     public User toEntity(UserDTO userDTO) {
-        User user = new User();
+
+        if (userDTO.getId() == null) {
+            User user = new User();
+
+            userDTO.getLogin();
+            userDTO.getEmail();
+            userDTO.getPhone();
+
+            return user;
+        }
+
+        User user = userService.getUser(userDTO.getId());
+
         user.setLogin(userDTO.getLogin());
         user.setEmail(userDTO.getEmail());
-        user.setPassword(userDTO.getPassword());
+        user.setPhone(userDTO.getPhone());
+
         return user;
     }
 }
