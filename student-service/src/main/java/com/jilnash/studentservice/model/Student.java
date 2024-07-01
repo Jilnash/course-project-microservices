8017528c551cda753b853978f607748a08b51f2a
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-07-02 01:04:54 +0500

    added student model
    Signed-off-by: = <=>

---
+++ student-service/src/main/java/com/jilnash/studentservice/model/Student.java
@@ -0,0 +1,40 @@
+package com.jilnash.studentservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students", schema = "course_project_students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false, unique = true)
+    private Long userId;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "surname", nullable = false, length = 50)
+    private String surname;
+
+    @Column(name = "link", nullable = false, length = 50)
+    private String link;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @UpdateTimestamp
+    private Date updatedAt;
+}
