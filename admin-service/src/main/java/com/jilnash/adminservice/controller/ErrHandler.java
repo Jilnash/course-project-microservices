f554b809a4fc7bd10969d24ad78ca8b24f2549ad
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-07-06 03:43:49 +0500

    added admin err handler
    Signed-off-by: = <=>

--- admin-service/src/main/java/com/jilnash/adminservice/controller/ErrHandler.java
+++ admin-service/src/main/java/com/jilnash/adminservice/controller/ErrHandler.java
@@ -1,4 +1,33 @@
 package com.jilnash.adminservice.controller;
 
+import com.jilnash.adminservice.dto.AppError;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
 public class ErrHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<?> handleNoSuchElementException(NoSuchElementException e) {
+        return ResponseEntity.badRequest().body(
+                new AppError(
+                        400,
+                        e.getMessage()
+                )
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body(
+                new AppError(
+                        400,
+                        e.getBindingResult().getAllErrors().get(0).getDefaultMessage()
+                )
+        );
+    }
 }
