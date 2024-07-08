567cfaf05db24133233a1a59db65859c328bef61
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-07-09 00:54:34 +0500

    added task repository
    Signed-off-by: = <=>

---
+++ course-service/src/main/java/com/jilnash/courseservice/repo/TaskRepo.java
@@ -0,0 +1,16 @@
+package com.jilnash.courseservice.repo;
+
+import com.jilnash.courseservice.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TaskRepo extends JpaRepository<Task, Long> {
+
+    Optional<Task> findByIdAndModuleIdAndModuleCourseId(Long id, Long moduleId, Long courseId);
+
+    List<Task> findAllByTitleAndModuleIdAndModuleCourseId(String title, Long moduleId, Long courseId);
+}
