f871f235666d271b462801ac33a9e626bfa7a93b
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-07-09 01:08:44 +0500

    added task service
    Signed-off-by: = <=>

---
+++ course-service/src/main/java/com/jilnash/courseservice/service/TaskServiceImpl.java
@@ -0,0 +1,38 @@
+package com.jilnash.courseservice.service;
+
+import com.jilnash.courseservice.model.Task;
+import com.jilnash.courseservice.repo.TaskRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    @Autowired
+    private TaskRepo taskRepo;
+
+    @Override
+    public List<Task> getTasks(String title, Long courseId, Long moduleId) {
+        return taskRepo.findAllByTitleAndModuleIdAndModuleCourseId(title, moduleId, courseId);
+    }
+
+    @Override
+    public Task getTask(Long id, Long moduleId, Long courseId) {
+        return taskRepo
+                .findByIdAndModuleIdAndModuleCourseId(id, moduleId, courseId)
+                .orElseThrow(() -> new NoSuchElementException(
+                                "Task not found with id: " + id +
+                                        " in module: " + moduleId +
+                                        " in course: " + courseId
+                        )
+                );
+    }
+
+    @Override
+    public Task save(Task task) {
+        return taskRepo.save(task);
+    }
+}
