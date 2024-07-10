faa9b4ba072147fa10b4c3f90c5b3f01cc736f25
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-07-11 03:03:51 +0500

    added homework controller
    Signed-off-by: = <=>

---
+++ homework-service/src/main/java/com/jilnash/homeworkservice/controller/ErrorHandler.java
@@ -0,0 +1,33 @@
+package com.jilnash.homeworkservice.controller;
+
+import com.jilnash.homeworkservice.dto.AppError;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return ResponseEntity.badRequest().body(
+                new AppError(
+                        400,
+                        ex.getBindingResult().getAllErrors().get(0).getDefaultMessage()
+                )
+        );
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<?> handleNoSuchElementException(NoSuchElementException ex) {
+        return ResponseEntity.badRequest().body(
+                new AppError(
+                        400,
+                        ex.getMessage()
+                )
+        );
+    }
+}
---
+++ homework-service/src/main/java/com/jilnash/homeworkservice/controller/v1/HomeworkController.java
@@ -0,0 +1,64 @@
+package com.jilnash.homeworkservice.controller.v1;
+
+import com.jilnash.homeworkservice.dto.AppResponse;
+import com.jilnash.homeworkservice.dto.HomeworkCreateDTO;
+import com.jilnash.homeworkservice.mapper.HomeworkMapper;
+import com.jilnash.homeworkservice.service.HomeworkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+
+@RestController
+@RequestMapping(value = "/api/v1/homeworks")
+@RequiredArgsConstructor
+public class HomeworkController {
+
+    @Autowired
+    private final HomeworkService homeworkService;
+
+    @Autowired
+    private final HomeworkMapper homeworkMapper;
+
+    @GetMapping
+    public ResponseEntity<?> getHomeworks(
+            @RequestParam(required = false) Long taskId,
+            @RequestParam(required = false) Long studentId,
+            @RequestParam(required = false) Boolean checked,
+            @RequestParam(required = false) Date createdAfter
+    ) {
+        return ResponseEntity.ok(
+                new AppResponse(
+                        200,
+                        "Homeworks fetched successfully",
+                        homeworkService.getHomeworks(taskId, studentId, checked, createdAfter)
+                )
+        );
+    }
+
+    @PutMapping
+    public ResponseEntity<?> createHomework(@Validated @RequestBody HomeworkCreateDTO homeworkDTO) {
+        return ResponseEntity.ok(
+                new AppResponse(
+                        200,
+                        "Homework updated successfully",
+                        homeworkService.saveHomework(homeworkMapper.toEntity(homeworkDTO))
+                )
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getHomework(@PathVariable Long id) {
+        return ResponseEntity.ok(
+                new AppResponse(
+                        200,
+                        "Homework fetched successfully",
+                        homeworkService.getHomework(id)
+                )
+        );
+    }
+}
+
