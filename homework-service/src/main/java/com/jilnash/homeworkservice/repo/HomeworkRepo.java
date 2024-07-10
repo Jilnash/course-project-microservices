5d6ca04b9cfed54e801b62430d9fc990616b649c
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-07-10 13:27:23 +0500

    added homework repository
    Signed-off-by: = <=>

---
+++ homework-service/src/main/java/com/jilnash/homeworkservice/repo/HomeworkRepo.java
@@ -0,0 +1,9 @@
+package com.jilnash.homeworkservice.repo;
+
+import com.jilnash.homeworkservice.model.Homework;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HomeworkRepo extends JpaRepository<Homework, Long> {
+}
