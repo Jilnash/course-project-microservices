5e314ee44b392e3a1a04238879c6f8261acee132
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-07-10 13:07:56 +0500

    added homework mapper
    Signed-off-by: = <=>

---
+++ homework-service/src/main/java/com/jilnash/homeworkservice/mapper/HomeworkMapper.java
@@ -0,0 +1,20 @@
+package com.jilnash.homeworkservice.mapper;
+
+import com.jilnash.homeworkservice.dto.HomeworkCreateDTO;
+import com.jilnash.homeworkservice.model.Homework;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HomeworkMapper {
+
+    public Homework toEntity(HomeworkCreateDTO homeworkDTO) {
+
+        return Homework.builder()
+                .studentId(homeworkDTO.getStudentId())
+                .taskId(homeworkDTO.getTaskId())
+                .audioLink(homeworkDTO.getAudioLink())
+                .videoLink(homeworkDTO.getVideoLink())
+                .checked(homeworkDTO.getChecked())
+                .build();
+    }
+}
