f89fb31b6fc57f209e717e55d95b52e7a0428f14
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-07-11 13:31:53 +0500

    added homework service
    Signed-off-by: = <=>

---
+++ hw-response-service/src/main/java/com/jilnash/hwresponseservice/HwResponseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jilnash.hwresponseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwResponseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwResponseServiceApplication.class, args);
+    }
+
+}
---
+++ hw-response-service/src/test/java/com/jilnash/hwresponseservice/HwResponseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jilnash.hwresponseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwResponseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
