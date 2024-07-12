f37224f8e85448b132e493277e9ffabc83d1273f
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-07-12 13:38:26 +0500

    added homework response mapper
    Signed-off-by: = <=>

---
+++ hw-response-service/src/main/java/com/jilnash/hwresponseservice/mapper/HwResponseMapper.java
@@ -0,0 +1,32 @@
+package com.jilnash.hwresponseservice.mapper;
+
+import com.jilnash.hwresponseservice.dto.HwResponseDTO;
+import com.jilnash.hwresponseservice.model.Comment;
+import com.jilnash.hwresponseservice.model.HwResponse;
+import com.jilnash.hwresponseservice.model.TimeRange;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class HwResponseMapper {
+
+    public HwResponse toEntity(HwResponseDTO dto) {
+
+        return HwResponse.builder()
+                .id(dto.getId())
+                .teacherId(dto.getTeacherId())
+                .homeworkId(dto.getHomeworkId())
+                .comments(dto.getComments().stream().map(commentDTO -> Comment.builder()
+                                .id(commentDTO.getId())
+                                .text(commentDTO.getText())
+                                .timeRange(TimeRange.builder()
+                                        .id(commentDTO.getTimeRange().getId())
+                                        .start(commentDTO.getTimeRange().getStart())
+                                        .end(commentDTO.getTimeRange().getEnd())
+                                        .build())
+                                .build())
+                        .collect(Collectors.toList()))
+                .build();
+    }
+}
