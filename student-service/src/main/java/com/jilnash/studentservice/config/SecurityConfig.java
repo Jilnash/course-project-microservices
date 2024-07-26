46f356ff5dce84e40a34782c518c754050bbe593
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-07-26 16:31:20 +0500

    configured security filter chain to accept request from gateway only
    Signed-off-by: = <=>

---
+++ student-service/src/main/java/com/jilnash/studentservice/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.jilnash.studentservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final String gatewayIp = "127.0.0.1"; //localhost
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers((request) -> request.getRemoteAddr().equals(gatewayIp)).permitAll()
+                        .anyRequest().denyAll()
+                )
+                .build();
+    }
+}
