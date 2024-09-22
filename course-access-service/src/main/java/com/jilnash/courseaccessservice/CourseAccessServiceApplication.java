d22e14f3894a2d37cc71e3f142b0bdd833301859
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-09-22 23:29:48 +0500

    created course service microservice

---
+++ course-access-service/src/main/java/com/jilnash/courseaccessservice/CourseAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jilnash.courseaccessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAccessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseAccessServiceApplication.class, args);
+    }
+
+}
---
+++ course-access-service/src/test/java/com/jilnash/courseaccessservice/CourseAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jilnash.courseaccessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseAccessServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
