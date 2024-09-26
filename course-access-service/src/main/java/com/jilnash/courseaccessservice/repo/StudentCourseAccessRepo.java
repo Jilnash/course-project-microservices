60b48c00c1b7b0874a4d14e0ca4ee198e9457121
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-09-26 18:59:53 +0500

    created query if a student has access to course currently

--- course-access-service/src/main/java/com/jilnash/courseaccessservice/repo/StudentCourseAccessRepo.java
+++ course-access-service/src/main/java/com/jilnash/courseaccessservice/repo/StudentCourseAccessRepo.java
@@ -4,6 +4,10 @@ import com.jilnash.courseaccessservice.model.StudentCourseAccess;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+
 @Repository
 public interface StudentCourseAccessRepo extends JpaRepository<StudentCourseAccess, Long> {
+
+    Boolean existsByStudentIdAndCourseIdAndStartDateBeforeAndEndDateAfter(Long studentId, String courseId, Date currentDate);
 }
