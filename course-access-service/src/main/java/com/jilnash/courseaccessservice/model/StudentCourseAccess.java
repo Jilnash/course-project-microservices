f7d9b7ed2c9279918c3a7f21d294f98acd01330d
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-09-26 18:34:18 +0500

    created course access model

---
+++ course-access-service/src/main/java/com/jilnash/courseaccessservice/model/StudentCourseAccess.java
@@ -0,0 +1,31 @@
+package com.jilnash.courseaccessservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.sql.Date;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "student_course_access")
+public class StudentCourseAccess {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long studentId;
+
+    private String courseId;
+
+    private Date startDate;
+
+    private Date endDate;
+}
