810f491d0c3655f0b79dcafeb3f03386cab5a3a3
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-09-26 19:12:45 +0500

    modified student's current course access query

--- course-access-service/src/main/java/com/jilnash/courseaccessservice/repo/StudentCourseAccessRepo.java
+++ course-access-service/src/main/java/com/jilnash/courseaccessservice/repo/StudentCourseAccessRepo.java
@@ -2,6 +2,7 @@ package com.jilnash.courseaccessservice.repo;
 
 import com.jilnash.courseaccessservice.model.StudentCourseAccess;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
@@ -9,5 +10,8 @@ import java.sql.Date;
 @Repository
 public interface StudentCourseAccessRepo extends JpaRepository<StudentCourseAccess, Long> {
 
+    @Query("""
+            select (count(s) > 0) from student_course_access s
+            where s.studentId = ?1 and s.courseId = ?2 and s.startDate < ?3 and s.endDate > ?3""")
     Boolean existsByStudentIdAndCourseIdAndStartDateBeforeAndEndDateAfter(Long studentId, String courseId, Date currentDate);
 }
