69b44f96fe0bbe9caaecf0b54c7f4bf44b881ff0
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-09-29 22:34:04 +0500

    enabled feign clients

--- course-service/src/main/java/com/jilnash/courseservice/CourseServiceApplication.java
+++ course-service/src/main/java/com/jilnash/courseservice/CourseServiceApplication.java
@@ -2,9 +2,11 @@ package com.jilnash.courseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @EnableNeo4jRepositories
+@EnableFeignClients
 @SpringBootApplication
 public class CourseServiceApplication {
 
