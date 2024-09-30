2cf03d202dd2c602d7a868268bde2037020051bc
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-09-30 17:19:52 +0500

    created course edit rights microservice

---
+++ course-rights-service/src/main/java/com/jilnash/courserightsservice/CourseRightsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jilnash.courserightsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRightsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseRightsServiceApplication.class, args);
+    }
+
+}
---
+++ course-rights-service/src/test/java/com/jilnash/courserightsservice/CourseRightsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jilnash.courserightsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRightsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
