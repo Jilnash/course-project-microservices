2889ee08244b857e5f4ab993a697feb0a76c9958
Aydar Kanatov <aydar-kanatov@mail.ru>
2024-10-27 12:36:50 +0500

    created StudentTaskComplete model

---
+++ progress-service/src/main/java/com/jilnash/progressservice/model/StudentTaskComplete.java
@@ -0,0 +1,29 @@
+package com.jilnash.progressservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.sql.Date;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity(name = "student_task_complete")
+public class StudentTaskComplete {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String studentId;
+
+    private String taskId;
+
+    private Date completeDate;
+}
