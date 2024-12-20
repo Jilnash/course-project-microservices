50d26e2c69d57eb60f74817f729e890d46905215
jilnash <aidar.kanatov@nu.edu.kz>
2024-12-20 23:07:42 +0500

    Substituted X-User-Email Header with X-User-Sub Header in custom gateway filter.
    X-User-Sub value is derived from `sub` claim in token

--- api-gateway/src/main/java/com/jilnash/apigateway/config/RouterConfig.java
+++ api-gateway/src/main/java/com/jilnash/apigateway/config/RouterConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.function.HandlerFilterFunction;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
-import static com.jilnash.apigateway.config.SetEmailHeaderFilter.setEmail;
+import static com.jilnash.apigateway.config.SetUserIdHeaderFilter.setIdHeader;
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.rewritePath;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
@@ -18,77 +18,77 @@ public class RouterConfig {
     @Bean
     public RouterFunction<ServerResponse> userService() {
         return route(path("/user-service/**"), http("http://localhost:8082"))
-                .filter(HandlerFilterFunction.ofRequestProcessor(setEmail()))
+                .filter(HandlerFilterFunction.ofRequestProcessor(setIdHeader()))
                 .filter(rewritePath("/user-service/(?<remaining>.*)", "/${remaining}"));
     }
 
     @Bean
     public RouterFunction<ServerResponse> adminService() {
         return route(path("/admin-service/**"), http("http://localhost:8085"))
-                .filter(HandlerFilterFunction.ofRequestProcessor(setEmail()))
+                .filter(HandlerFilterFunction.ofRequestProcessor(setIdHeader()))
                 .filter(rewritePath("/admin-service/(?<remaining>.*)", "/${remaining}"));
     }
 
     @Bean
     public RouterFunction<ServerResponse> teacherService() {
         return route(path("/teacher-service/**"), http("http://localhost:8084"))
-                .filter(HandlerFilterFunction.ofRequestProcessor(setEmail()))
+                .filter(HandlerFilterFunction.ofRequestProcessor(setIdHeader()))
                 .filter(rewritePath("/teacher-service/(?<remaining>.*)", "/${remaining}"));
     }
 
     @Bean
     public RouterFunction<ServerResponse> studentService() {
         return route(path("/student-service/**"), http("http://localhost:8083"))
-                .filter(HandlerFilterFunction.ofRequestProcessor(setEmail()))
+                .filter(HandlerFilterFunction.ofRequestProcessor(setIdHeader()))
                 .filter(rewritePath("/student-service/(?<remaining>.*)", "/${remaining}"));
     }
 
     @Bean
     public RouterFunction<ServerResponse> courseService() {
         return route(path("/course-service/**"), http("http://localhost:8086"))
-                .filter(HandlerFilterFunction.ofRequestProcessor(setEmail()))
+                .filter(HandlerFilterFunction.ofRequestProcessor(setIdHeader()))
                 .filter(rewritePath("/course-service/(?<remaining>.*)", "/${remaining}"));
     }
 
     @Bean
     public RouterFunction<ServerResponse> fileService() {
         return route(path("/file-service/**"), http("http://localhost:8087"))
-                .filter(HandlerFilterFunction.ofRequestProcessor(setEmail()))
+                .filter(HandlerFilterFunction.ofRequestProcessor(setIdHeader()))
                 .filter(rewritePath("/file-service/(?<remaining>.*)", "/${remaining}"));
     }
 
     @Bean
     public RouterFunction<ServerResponse> hwService() {
         return route(path("/hw-service/**"), http("http://localhost:8088"))
-                .filter(HandlerFilterFunction.ofRequestProcessor(setEmail()))
+                .filter(HandlerFilterFunction.ofRequestProcessor(setIdHeader()))
                 .filter(rewritePath("/hw-service/(?<remaining>.*)", "/${remaining}"));
     }
 
     @Bean
     public RouterFunction<ServerResponse> responseService() {
         return route(path("/response-service/**"), http("http://localhost:8089"))
-                .filter(HandlerFilterFunction.ofRequestProcessor(setEmail()))
+                .filter(HandlerFilterFunction.ofRequestProcessor(setIdHeader()))
                 .filter(rewritePath("/response-service/(?<remaining>.*)", "/${remaining}"));
     }
 
     @Bean
     public RouterFunction<ServerResponse> courseAccessService() {
         return route(path("/course-access-service/**"), http("http://localhost:8090"))
-                .filter(HandlerFilterFunction.ofRequestProcessor(setEmail()))
+                .filter(HandlerFilterFunction.ofRequestProcessor(setIdHeader()))
                 .filter(rewritePath("/course-access-service/(?<remaining>.*)", "/${remaining}"));
     }
 
     @Bean
     public RouterFunction<ServerResponse> courseRightsService() {
         return route(path("/course-rights-service/**"), http("http://localhost:8091"))
-                .filter(HandlerFilterFunction.ofRequestProcessor(setEmail()))
+                .filter(HandlerFilterFunction.ofRequestProcessor(setIdHeader()))
                 .filter(rewritePath("/course-rights-service/(?<remaining>.*)", "/${remaining}"));
     }
 
     @Bean
     public RouterFunction<ServerResponse> progressService() {
         return route(path("/progress-service/**"), http("http://localhost:8092"))
-                .filter(HandlerFilterFunction.ofRequestProcessor(setEmail()))
+                .filter(HandlerFilterFunction.ofRequestProcessor(setIdHeader()))
                 .filter(rewritePath("/progress-service/(?<remaining>.*)", "/${remaining}"));
     }
 }
--- api-gateway/src/main/java/com/jilnash/apigateway/config/SetUserIdHeaderFilter.java
+++ api-gateway/src/main/java/com/jilnash/apigateway/config/SetUserIdHeaderFilter.java
@@ -6,16 +6,16 @@ import org.springframework.web.servlet.function.ServerRequest;
 
 import java.util.function.Function;
 
-public class SetEmailHeaderFilter {
+public class SetUserIdHeaderFilter {
 
-    public static Function<ServerRequest, ServerRequest> setEmail() {
-        return request -> ServerRequest.from(request).header("X-User-Email", getEmailFromToken()).build();
+    public static Function<ServerRequest, ServerRequest> setIdHeader() {
+        return request -> ServerRequest.from(request).header("X-User-Sub", getIdFromToken()).build();
     }
 
-    private static String getEmailFromToken() {
+    private static String getIdFromToken() {
         var auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null && auth.getPrincipal() instanceof Jwt jwt)
-            return jwt.getClaimAsString("email");
+            return jwt.getClaimAsString("sub");
         return null;
     }
 }
