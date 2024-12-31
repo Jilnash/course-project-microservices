da3a0dcc3af3d1ee97e5872fe8b615fbb44e257a
jilnash <aidar.kanatov@nu.edu.kz>
2024-12-31 12:58:53 +0500

    inited `task-file-requirements-service` to separate from `course-service`

---
+++ task-file-requirements-service/src/main/java/com/jilnash/taskfilerequirementsservice/TaskFileRequirementsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jilnash.taskfilerequirementsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFileRequirementsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskFileRequirementsServiceApplication.class, args);
+    }
+
+}
---
+++ task-file-requirements-service/src/test/java/com/jilnash/taskfilerequirementsservice/TaskFileRequirementsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jilnash.taskfilerequirementsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskFileRequirementsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
