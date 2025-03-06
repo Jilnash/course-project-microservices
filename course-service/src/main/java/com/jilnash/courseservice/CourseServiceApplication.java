b58e93de04c2909f7ff8334a0c6ce42b18a1bf31
jilnash <aidar.kanatov@nu.edu.kz>
2025-03-07 03:21:36 +0500

    - enabled async, since latency did not change

--- course-service/src/main/java/com/jilnash/courseservice/CourseServiceApplication.java
+++ course-service/src/main/java/com/jilnash/courseservice/CourseServiceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableNeo4jRepositories
 @EnableCaching
+@EnableAsync
 @EnableFeignClients
 @SpringBootApplication
 public class CourseServiceApplication {
--- homework-service/src/main/java/com/jilnash/homeworkservice/HomeworkServiceApplication.java
+++ homework-service/src/main/java/com/jilnash/homeworkservice/HomeworkServiceApplication.java
@@ -2,9 +2,13 @@ package com.jilnash.homeworkservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableFeignClients
+@EnableAsync(proxyTargetClass = true)
+@EnableCaching(proxyTargetClass = true)
 @SpringBootApplication
 public class HomeworkServiceApplication {
 
