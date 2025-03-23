c254c5ac2cc34fdb6880f51e0a3eafc80ddf0121
jilnash <aidar.kanatov@nu.edu.kz>
2025-03-23 17:16:12 +0500

    continuation to previous commit

---
+++ config-server/src/test/java/com/jilnash/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jilnash.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
