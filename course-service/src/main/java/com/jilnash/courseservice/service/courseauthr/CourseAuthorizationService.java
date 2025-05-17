542864aadddb2620ffda865a3dd0b85d2eabbf16
jilnash <aidar.kanatov@nu.edu.kz>
2025-05-17 15:59:12 +0500

    Enhance documentation for CourseAuthorizationService methods and add detailed Javadoc comments for the class and methods

--- course-service/src/main/java/com/jilnash/courseservice/service/courseauthr/CourseAuthorizationService.java
+++ course-service/src/main/java/com/jilnash/courseservice/service/courseauthr/CourseAuthorizationService.java
@@ -19,6 +19,10 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+/**
+ * Service responsible for managing and validating user access and rights to courses.
+ * Utilizes gRPC clients to communicate with external services for student access and teacher rights validation.
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -33,6 +37,15 @@ public class CourseAuthorizationService {
     private TeacherRightsServiceGrpc.TeacherRightsServiceBlockingStub teacherRightsServiceBlockingStub;
 
 
+    /**
+     * Validates the access rights of a user for the specified course. The method checks if the user
+     * has student-level access to the course or teacher-level rights with at least "READ" permissions.
+     * If neither condition is met, an exception is thrown.
+     *
+     * @param courseId the unique identifier of the course
+     * @param userId   the unique identifier of the user
+     * @throws RuntimeException if access validation fails or if an error occurs while checking access rights
+     */
     public void validateUserAccess(String courseId, String userId) {
 
         try {
@@ -44,7 +57,19 @@ public class CourseAuthorizationService {
         }
     }
 
-    public void validateTeacherCourseRights(String courseId, String teacherId, List<String> rights) {
+    /**
+     * Validates whether a teacher has the specified rights for a given course. If the validation fails, a
+     * {@link UsernameNotFoundException} is thrown. Handles any execution errors during the validation
+     * process by wrapping them in a {@link RuntimeException}.
+     *
+     * @param courseId  the unique identifier of the course
+     * @param teacherId the unique identifier of the teacher
+     * @param rights    a list of rights to check for the teacher
+     * @throws UsernameNotFoundException if the teacher does not have the specified rights for the course
+     * @throws RuntimeException          if an error occurs during the execution of the rights validation
+     */
+    public void validateTeacherCourseRights
+    (String courseId, String teacherId, List<String> rights) {
 
         try {
             if (!getTeacherHasCourseRights(courseId, teacherId, rights).get())
@@ -54,6 +79,15 @@ public class CourseAuthorizationService {
         }
     }
 
+    /**
+     * Checks if a teacher has specific rights for a given course by interacting with the course-rights-service
+     * through an asynchronous gRPC call.
+     *
+     * @param courseId the unique identifier of the course
+     * @param teacherId the unique identifier of the teacher
+     * @param rights a list of rights to check for the teacher
+     * @return a CompletableFuture containing a Boolean value indicating whether the teacher has the specified rights
+     */
     @Async
     protected CompletableFuture<Boolean> getTeacherHasCourseRights(String courseId, String teacherId, List<String> rights) {
         Span span = tracer.spanBuilder("getTeacherHasCourseRights").startSpan();
@@ -78,6 +112,16 @@ public class CourseAuthorizationService {
         }
     }
 
+    /**
+     * Validates if a student has access to a specific course. If the access check fails, a
+     * {@link UsernameNotFoundException} is thrown. Handles any execution errors during the validation
+     * process by wrapping them in a {@link RuntimeException}.
+     *
+     * @param courseId the unique identifier of the course
+     * @param studentId the unique identifier of the student
+     * @throws UsernameNotFoundException if the student does not have access to the course
+     * @throws RuntimeException if an error occurs during the execution of the access validation
+     */
     public void validateStudentCourseAccess(String courseId, String studentId) {
 
         try {
@@ -88,6 +132,13 @@ public class CourseAuthorizationService {
         }
     }
 
+    /**
+     * Checks if a student has access to a specific course by communicating with an external course access service.
+     *
+     * @param courseId the unique identifier of the course
+     * @param studentId the unique identifier of the student
+     * @return a CompletableFuture containing a Boolean value indicating whether the student has access to the course
+     */
     @Async
     protected CompletableFuture<Boolean> getStudentCourseAccess(String courseId, String studentId) {
         Span span = tracer.spanBuilder("getStudentCourseAccess").startSpan();
